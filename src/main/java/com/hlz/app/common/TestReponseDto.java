fe6a41a964a5d8c7ae2a9731d42b36de88daa045
NewHms <37338418+NewHms@users.noreply.github.com>
2018-06-11 22:34:44 +0800

    删除title2

--- src/main/java/com/hlz/app/common/TestReponseDto.java
+++ src/main/java/com/hlz/app/common/TestReponseDto.java
@@ -7,16 +7,6 @@ public class TestReponseDto extends ReponseDto {
 
     private List<?> Data;
 
-    public List<?> getTitle2() {
-        return title2;
-    }
-
-    public void setTitle2(List<?> title2) {
-        this.title2 = title2;
-    }
-
-    private List<?> title2;
-
     public List<?> getData() {
         return Data;
     }
